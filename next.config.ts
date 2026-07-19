e040b5b468820291eca98b22f92bc331982389d8
alam8718 <mahabubalam3004@gmail.com>
2026-07-19 16:33:31 +0600

    bud fixed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 /** Next.js application configuration. */
 const nextConfig: NextConfig = {
   /* config options here */
+};
+module.exports = {
+  allowedDevOrigins: ["192.168.100.245"],
 };
 
 export default nextConfig;
